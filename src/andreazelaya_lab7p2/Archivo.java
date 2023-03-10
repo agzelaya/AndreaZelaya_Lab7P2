d4f6ef36aac813844d71eecc77c7680c662cd6d3
Andrea Zelaya <agzelaya.f@gmail.com>
2023-03-10 14:52:52 -0600

    agregar archivos y carpetas

---
+++ src/andreazelaya_lab7p2/Archivo.java
@@ -0,0 +1,58 @@
+package andreazelaya_lab7p2;
+
+public class Archivo {
+    private String nombre;
+    private String link;
+    private String extension;
+    private double size;
+
+    public Archivo(String nombre, String link, String extension, double size) {
+        this.nombre = nombre;
+        this.link = link;
+        this.extension = extension;
+        this.size = size;
+    }
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Archivo{" + "nombre=" + nombre + ", link=" + link + ", extension=" + extension + ", size=" + size + '}';
+    }
+
+    
+    
+    
+}
